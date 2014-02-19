6daf8e83e1b24ee83ebff72d1730bccfcc3f287e
Charles Griffin <cgriffin@indatus.com>
2014-02-18 23:54:01 -0500

    fixed a few errors

--- app/models/Transaction.php
+++ app/models/Transaction.php
@@ -4,6 +4,7 @@ class Transaction extends Eloquent
 {
     const EXPENSE = 1;
     const INCOME = 2;
+    const RECURRING = 3;
 
     protected $fillable = ['transaction_type_id', 'amount'];
 
@@ -14,39 +15,30 @@ class Transaction extends Eloquent
 
     }
 
-    public function performComplicatedMethodWithIfElse()
-    {
-        //oh and deductAmount has some conditionals too!
-        $this->account->deductAmount();
-
-        if($this->account->amount < 0) {
-            //a bunch of if-else's in sendAlert
-            //to figure out the type of alert
-            $this->account->sendAlert();
-        }
-    }
-
-    public function performOtherComplicatedMethodWithIfElse()
-    {
-        
-        if($this->amount > 1000 && $this->withInTodaysRange()) {
-            //oh and addAmount has some conditionals too!
-            $this->account->addAmount(); 
-            return;   
-        }
-
-        return 'please contact your bank cuz you spending too much money';
-    }
-
     public function processNewTransaction()
     {
+        $alert = new Alert;
+
         if($this->transaction_type_id == self::EXPENSE) {
 
-            $this->performComplicatedMethodWithIfElse();
+            $this->account->substractAmount($this->amount);
+
+            if($this->account->amount < 0) {
+                
+                return $alert->sendAlert('negative_balance');
+            }
+
+            return $alert->sendAlert('success');
 
         } elseif ($this->transaction_type_id == self::INCOME) {
 
-            $this->performOtherComplicatedMethodWithIfElse();
+            $this->account->addAmount($this->amount);
+                
+            return $alert->sendAlert('success');
+
+        } elseif ($this->transaction_type_id == self::RECURRING) {
+
+            return "No Deductions Quite yet, it will take three weeks";
 
         } else {
 
